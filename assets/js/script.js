c0260df9535406bf6bac09cbc19ab6cac7b8a718
Natalia Devia <natidevia@gmail.com>
2025-08-02 17:14:15 -0400

    actualiza funcionalidad agregarTarea y agrega validaciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,39 @@
 let listaTareas = [];
 
+const seccionAgregarTarea = document.querySelector(".container-input");
 const botonAgregarTarea = document.getElementById("boton-agregar-tarea");
-
 const containerTareas = document.querySelector(".container-tareas");
+const input = document.getElementById("nueva-tarea");
+
+const validarTarea = (tarea) => {
+  // eliminar mensajes de advertencia existentes
+  const mensajeExistente = seccionAgregarTarea.querySelector(".invalid");
+  if (mensajeExistente) {
+    mensajeExistente.remove();
+  }
+
+  const validacion = document.createElement("p");
+  validacion.classList.add("text-danger", "text-start", "invalid");
+  
+  // validar que no se ingrese una tarea vacía
+  if (tarea.length === 0) {
+    validacion.textContent = "No se puede ingresar una tarea vacía";
+    seccionAgregarTarea.appendChild(validacion);
+    return false;
+  }
+  // validar que no se ingrese una tarea repetida
+  if (listaTareas.includes(tarea)) {
+    validacion.textContent = "No puedes ingresar 2 veces la misma tarea";
+    seccionAgregarTarea.appendChild(validacion);
+    return false;    
+  }
+
+  return true;
+}
 
 const agregarTarea = (tarea) => {
-  if (tarea.length != 0) {
-    if (listaTareas.includes(tarea)) {
-      console.log("No puedes ingresar 2 veces la misma tarea");
-    } else {
-      listaTareas.push(tarea);
-    }
-  } else {
-    console.log("No se puede ingresar una tarea vacía");
+  if (validarTarea(tarea)) {
+    listaTareas.push(tarea);
   }
 }
 
@@ -82,10 +103,35 @@ const crearTarea = (tarea) => {
 
 botonAgregarTarea.addEventListener("click", (event) => {
   event.preventDefault();
-  const input = document.getElementById("nueva-tarea");
   const nuevaTarea = input.value;
   agregarTarea(nuevaTarea);
   input.value = "";
   mostrarTareas(listaTareas);
 });
 
+
+
+
+
+
+/*
+
+
+
+Implementa la capacidad de eliminar tareas de la lista al hacer clic en ellas. Esto requerirá recorrer el arreglo y actualizarlo correctamente.
+
+
+
+
+eliminarTarea(): Función que elimina una tarea de la lista.
+
+
+Utilizar objetos preconstruidos del lenguaje JavaScript para resolver un problema
+En tu aplicación, deberás hacer uso de al menos un objeto preconstruido de JavaScript, como:
+
+Date: para mostrar la fecha actual cuando se agrega una tarea.
+
+Math: para generar un número aleatorio como identificador único de cada tarea.
+
+Incluye un archivo README.md con una descripción clara del proyecto, cómo ejecutar el código, y las tecnologías utilizadas.
+*/
